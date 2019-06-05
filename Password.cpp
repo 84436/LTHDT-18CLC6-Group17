47094e8efc78992eeaa20fc514fe82eeb0519c75
84436 <thien.bui.84436@gmail.com>
2019-06-05 20:29:09 +0700

    Update GITIGNORE

---
+++ Password.cpp
@@ -0,0 +1,29 @@
+﻿#include "Password.h"
+
+void Password::GetPassword()
+{
+	// mask field
+	
+	char c = 0;
+	while (c = cin.get()) {
+		if (c == 8) { // BACKSPACE
+
+		}
+		else if (c == 13) { // RETURN
+
+		}
+		else if (c == 0 || c == 224) { // CONTROL CHARS
+
+		}
+		else {
+
+		}
+	}
+
+	// băm
+}
+
+bool Password::operator==(const Password& x)
+{
+	return (data == x.data);
+}
---
+++ Password.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+// Hàm băm SHA-256
+// được cung cấp bởi http://www.zedwood.com/article/cpp-sha256-function
+// CC-BY-SA 3.0
+#include "sha256/sha256.h"
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Password
+{
+	private:
+		string data;
+
+	public:
+		void GetPassword(); // lấy và băm pass, đưa vào data
+		bool operator== (const Password& x); // so sánh
+};
