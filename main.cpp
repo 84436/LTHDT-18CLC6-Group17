334b98bf01979d83ff951b8afe59fe374af2aa18
Bùi Văn Thiện <thien.bui.84436@gmail.com>
2019-07-03 22:20:05 +0700

    tạo file Shipper; Password --> Login; sơ lược main mẫu

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include "Login.h"
+#include "Product.h"
+#include "Order.h"
+#include "Buyer.h"
+#include "Seller.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	vector<Order> Orders;
+	vector<Product> Products;
+	vector<Buyer> Buyers;
+	vector<Seller> Sellers;
+	
+	return 0;
+}
